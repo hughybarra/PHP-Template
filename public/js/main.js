db48da6c0c149d8f4de5b6c8796df95c7754732c
Hugh Ybarra <hugh.ybarra@gmaillcom>
2014-07-18 00:33:32 -0400

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,13 @@ $( document ).ready(function() {
   	// Delete when ready
   	console.log("main.js is loading");
 
+
+  	// hooking up the bxslider
+	$('.bxslider').bxSlider({
+		auto: true,
+		controls: false,
+		autoHover: true,
+	});
+
+
 });// end doc ready function
